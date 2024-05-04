97a6ef0a9fa78bc9635e5662eace08828c112bfc
Sveske_Juice <carl.benjamin.dreyer@gmail.com>
2024-05-04 11:44:47 +0200

    circuit transmit test

--- src/main.c
+++ src/main.c
@@ -16,6 +16,7 @@
 #define IN3 3
 
 uint8_t circuitStatus = 0;
+char statePayloadBuf[9];
 
 MQTTClient client;
 MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
@@ -31,9 +32,23 @@ uint8_t getCircuitState() {
     return newState;
 }
 
+char* printState(uint8_t state) {
+    int bitPos = 0;
+    for (int i = 1 << 7; i != 0; i = i >> 1) {
+        if ((state & i) > 0)
+            statePayloadBuf[bitPos++] = '1';
+        else
+            statePayloadBuf[bitPos++] = '0';
+    }
+    statePayloadBuf[8] = '\0';
+    return statePayloadBuf;
+}
+
 int main(int argc, char *argv[])
 {
+    wiringPiSetup();
     pinMode(IN0, INPUT);
+    pullUpDnControl(IN0, PUD_DOWN);
     pinMode(IN1, INPUT);
     pinMode(IN2, INPUT);
     pinMode(IN3, INPUT);
@@ -69,10 +84,11 @@ int main(int argc, char *argv[])
         // Something changed... we should notify subscribers
         if (circuitStatus != newCircuitState) {
             circuitStatus = newCircuitState;
-            printf("state: %d\n", newCircuitState);
-            const char * payload = "HEELOO";
-            pubmsg.payload = (void*)payload;
-            pubmsg.payloadlen = (int)strlen(payload);
+            printState(circuitStatus);
+            printf("state: %s\n", statePayloadBuf);
+
+            pubmsg.payload = statePayloadBuf;
+            pubmsg.payloadlen = (int)strlen(statePayloadBuf);
             pubmsg.qos = 1;
             pubmsg.retained = 0;
             if ((rc = MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token)) != MQTTCLIENT_SUCCESS)
@@ -83,15 +99,14 @@ int main(int argc, char *argv[])
 
             printf("Waiting for up to %d seconds for publication of %s\n"
                     "on topic %s for client with ClientID: %s\n",
-                    (int)(TIMEOUT/1000), payload, TOPIC, CLIENTID);
+                    (int)(TIMEOUT/1000), statePayloadBuf, TOPIC, CLIENTID);
             rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
             printf("Message with delivery token %d delivered\n", token);
-
-            if ((rc = MQTTClient_disconnect(client, 10000)) != MQTTCLIENT_SUCCESS)
-                printf("Failed to disconnect, return code %d\n", rc);
-            MQTTClient_destroy(&client);
         }
     }
+    if ((rc = MQTTClient_disconnect(client, 10000)) != MQTTCLIENT_SUCCESS)
+        printf("Failed to disconnect, return code %d\n", rc);
+    MQTTClient_destroy(&client);
 
     return EXIT_SUCCESS;
 }
